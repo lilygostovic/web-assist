4678e8c2932ae54e594dd4b3c623be98b63025b9
Zachary Yang <zachary971011@gmail.com>
2024-07-31 00:28:26 -0400

    manually update huggingface-hub version

--- Backend/src/setup.sh
+++ Backend/src/setup.sh
@@ -1,6 +1,7 @@
 apt-get update
 apt-get -y upgrade
 pip install -r requirements.txt
+pip install --upgrade huggingface-hub==0.24.0
 apt-get -y install git
 FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE pip install "flash-attn>=2.3.0" --no-build-isolation
 
